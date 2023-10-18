52cd0193da1c95b8f0014b25d3328cf56b3d312a
Raghavendra Goud <61324448+Hackmacha007@users.noreply.github.com>
2023-10-18 20:16:17 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,29 @@ $(document).ready(function(){
   });
 });
 
+
+let slideIndex = 1;
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+showSlides(slideIndex);
+
